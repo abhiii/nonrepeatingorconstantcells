5010a5dec2b95adb7e3514b1a5651a5eb5161688
abhiii <abhii.reddy6@gmail.com>
2013-06-15 11:11:57 +0530

    added my class for constant cells

---
+++ NonRepeatingCellsExample/AbhiCell.h
@@ -0,0 +1,13 @@
+//
+//  AbhiCell.h
+//  NonRepeatingCellsExample
+//
+//  Created by Hadi Hatunoglu on 15/06/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AbhiCell : UITableViewCell
+
+@end
---
+++ NonRepeatingCellsExample/NonRepeatingCellsExampleViewController.h
@@ -0,0 +1,22 @@
+//
+//  NonRepeatingCellsExampleViewController.h
+//  NonRepeatingCellsExample
+//
+//  Created by Mugunth Kumar M on 23/8/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HeaderCell.h"
+#import "BodyCell.h"
+#import "FooterCell.h"
+#import "AbhiCell.h"
+
+@interface NonRepeatingCellsExampleViewController : UIViewController
+
+@property (nonatomic, strong) IBOutlet UITableView *mainTable;
+@property (nonatomic, strong) IBOutlet HeaderCell *headerCell;
+@property (nonatomic, strong) IBOutlet BodyCell *bodyCell;
+@property (nonatomic, strong) IBOutlet FooterCell *footerCell;
+@property (nonatomic, strong) IBOutlet AbhiCell *abhiCell;
+@end
